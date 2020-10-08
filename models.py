c7620f0c55e3aa698fb7b520ea871d4c4a8fd056
Kyle R Hassold <41242317+KyleHassold@users.noreply.github.com>
2020-10-08 16:35:44 -0400

    Created Generator Model

---
+++ models.py
@@ -0,0 +1,35 @@
+from keras.models import Sequential
+from keras.layers import Dense, Reshape, Conv2D, Conv2DTranspose, LeakyReLU
+
+#Define Generator Model
+def define_generator(latent_dim):
+    model = Sequential()
+
+    # foundation for 6x5 image
+    n_nodes = 128 * 6 * 5
+    model.add(Dense(n_nodes, input_dim=latent_dim))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Reshape((6, 5, 128)))
+
+    # upsample to 12x10
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(LeakyReLU(alpha=0.2))
+
+    # upsample to 24x20
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(LeakyReLU(alpha=0.2))
+
+    # upsample to 48x40
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(LeakyReLU(alpha=0.2))
+
+    # upsample to 96x80
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(LeakyReLU(alpha=0.2))
+
+    # upsample to 192x160
+    model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Conv2D(3, (6,5), activation='sigmoid', padding='same'))
+
+    return model
