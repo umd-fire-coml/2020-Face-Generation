3824b52f845aff9fd12e57575a2f1d32490c53b3
Kyle R Hassold <41242317+KyleHassold@users.noreply.github.com>
2020-10-13 16:38:25 -0400

    Change for upload

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
-tfds.list_builders()[:20]
+#tfds.list_builders()[:20]
 
 #change data_dir to where you want dataset to be stored
 ds = tfds.load('celeb_a', data_dir=".")
