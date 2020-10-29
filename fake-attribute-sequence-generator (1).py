985370860978fcc2d031b2739462508a07a9721a
Khushibhansali <30347957+Khushibhansali@users.noreply.github.com>
2020-10-29 16:53:15 -0400

    Update fake-attribute-sequence-generator (1).py

--- fake-attribute-sequence-generator (1).py
+++ fake-attribute-sequence-generator (1).py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import tensorflow as tf
 from tensorflow import keras
@@ -26,7 +21,7 @@ class FakeAttributeSequenceGenerator(keras.utils.Sequence):
         return list1, list2
 
 
-# In[ ]:
+
 
 
 
