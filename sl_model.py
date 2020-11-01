c8cfc695a679186ad12e665730c5ad2a0166f58f
SeanHuang18 <seanhuang0218@gmail.com>
2020-11-01 16:15:02 -0500

    Update sl_model.py
    fixed variable names

--- sl_model.py
+++ sl_model.py
@@ -1,11 +1,9 @@
 #What else should we take in as input
-def save_model(epoch_num):
-    checkpoint_path = "model/"
-    self.generator.save_weights('models/epoch_%s.h5' % (epoch))
+def save_model(model, path='model/', epoch_num=0):
+    model.save_weights(path + 'epoch_%s.h5' % epoch_num)
 
 
 def load_model(path):
-    model.load_weights(checkpoint_path)
-    #what info do we need?
-    model.evaluate(test_images,  test_labels, verbose=2)
+    model.load_weights(path)
+    print("Loaded")
     return model
